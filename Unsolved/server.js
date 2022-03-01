3d1040731c20d6dbc70629ad053e674f761f8e95
Akash Limbu <akashwanem@hotmail.com>
2022-03-01 19:58:35 +0000

    done

---
+++ Unsolved/server.js
@@ -0,0 +1,22 @@
+// TODO: Import express
+const express = require('express');
+
+
+// TODO: Import 'terms.json' file
+const PORT = 3001;
+const termData = require('./terms.json');
+
+// TODO: Initialize app variable
+const app = express();
+
+// TODO: Create a route for a GET request that will return the content of our `terms.json` file
+app.get('/term', (req, res) => {
+  res.json(termData)
+});
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
